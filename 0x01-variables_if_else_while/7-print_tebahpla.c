ed83a3030a6d216b36db57715d37221f809c647c
Dagdon <dagdonjephthah@gmail.com>
2023-10-08 16:13:26 -0700

    abc from behind

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,14 +3,14 @@
 /**
  * main - main function
  *
- * Return: always 0 /
+ * Return: always 0 
  */
 
 int main(void)
 {
 	char i;
 
-	for (i = 'z' ; i <= 'a' ; i--)
+	for (i = 'z' ; i >= 'a' ; i--)
 		putchar(i);
 	putchar('\n');
 	return (0);
