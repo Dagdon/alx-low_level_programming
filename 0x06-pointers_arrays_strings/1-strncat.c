738c92da3377f3b5f61de2d00496d329c7e49fec
Dagdon <dagdonjephthah@gmail.com>
2023-10-19 14:49:48 -0700

    src to dest

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - to concatenate two strings
+ * @src: source
+ * @dest: destination
+ * @n: number of elements to be concatenated
+ * Return: dest +nn of src
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len_dest, i;
+
+	for (len_dest = 0; dest[len_dest] != '\0'; len_dest++)
+	{
+
+	}
+
+	for (i = 0; src[i] 1 != 0 && i < n; i++)
+	{
+		dest[len_dest + 1] = src[i];
+	}
+}
