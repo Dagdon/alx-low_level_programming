a258501183094378b9c99dcf22873674234bd54c
Dagdon <dagdonjephthah@gmail.com>
2023-10-27 16:26:37 -0700

    empty files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase letters
+ * @c: parameter
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @c: parameter
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
