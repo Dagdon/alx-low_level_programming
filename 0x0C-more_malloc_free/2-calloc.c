e375c552ed8d9893bd657dcd753f3f0b679b39b2
Dagdon <dagdonjephthah@gmail.com>
2023-11-02 15:52:57 -0700

    allocate memory using calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmeb: number of memoery blocks
+ * @size: size of element
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *t;
+	size_t y;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	t = malloc(nmemb * size);
+	if (t == NULL)
+		return (NULL);
+	for (y = 0; y < (nmemb * size); y++)
+		t[y] = 0;
+	return (t);
+}
