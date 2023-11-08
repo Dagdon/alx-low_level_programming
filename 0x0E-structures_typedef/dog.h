9763e74b348e216df6ffca27e73a71bdc1676f8d
Dagdon <dagdonjephthah@gmail.com>
2023-11-07 17:41:16 -0800

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure dog
+ * @owner: dog's owner
+ * @name: dog's name
+ * @age: dog's age
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
