65d9d83aeca1e3644ad285847cfd34a434a84742
Dagdon <dagdonjephthah@gmail.com>
2023-11-21 16:27:54 -0800

    return number of elements in a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,20 @@
 #include "lists.h"
 
 /**
- * list
+ * listint_len - returns number of elements in a list
+ * @h: pointer to header
+ * Return: number of elements
+ */
+
 size_t listint_len(const listint_t *h)
+{
+	size_t coun = 0;
+
+	if (h == NULL)
+		return (0);
+	for (coun = 0; h != NULL; coun++)
+	{
+		h = h->next;
+	}
+	return (coun);
+}
