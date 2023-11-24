977f4902109a4967f427c0b78f3b2a63da145320
Dagdon <dagdonjephthah@gmail.com>
2023-11-24 14:26:23 -0800

    prints binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_bi - prints binary representation of a number recursively
+ * @n: the number
+ */
+
+void print_bi(unsigned int n)
+{
+	if (n == 0)
+		return;
+	print_bi(n >> 1);
+	if ((n & 1) == 1)
+		_putchar('1');
+	if ((n & 1) == 0)
+		_putchar('0');
+}
+
+/**
+ * print_binary - prints binary
+ * @n: decimal
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		print_bi(n);
+	}
+}
