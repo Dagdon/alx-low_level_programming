823303dc050730b8fba80af85437c0e085673fd8
Dagdon <dagdonjephthah@gmail.com>
2024-04-03 03:26:19 -0700

    checks for upper case letters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,17 @@
 #include "main.h"
-
 /**
- * _isupper - checks for upper case letter
- * @c: parameter
- * Return: anything
+ * _isupper - checks for uppercase letters
+ *  @c: character
+ *  Return: 1 or 0
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c >= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
