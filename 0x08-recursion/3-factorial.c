9bc5b87c8deb9db4ec39c4058d40b6818ec45a2c
Dagdon <dagdonjephthah@gmail.com>
2024-04-04 07:30:44 -0700

    factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int factorial - returns factoria of a number
+ * @n: number
+ * Return: number
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,3 @@
+#include "main.h"
+/**
+ *  _pow_recursion 
