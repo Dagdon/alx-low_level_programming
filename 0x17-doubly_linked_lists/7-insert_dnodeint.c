3461492197e37255d914b8f522b2003f2557aa82
Dagdon <dagdonjephthah@gmail.com>
2024-07-04 08:11:28 -0700

    add a node at a given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,12 +15,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+
 	for (count = 1; count < idx; count++)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
 			return (NULL);
-		tmp = tmp->next;
 	}
 
 	if (tmp->next == NULL)
@@ -29,6 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
 	new->prev = tmp;
 	new->next = tmp->next;
